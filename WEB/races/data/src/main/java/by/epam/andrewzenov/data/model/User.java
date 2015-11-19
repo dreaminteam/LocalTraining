05cb073117bbce692355e8eae179e40c63b7e670
Andrew Zenov <andrew.work@tut.by>
2015-11-19 15:59:34 +0200

    add project

---
+++ WEB/races/data/src/main/java/by/epam/andrewzenov/data/model/User.java
@@ -0,0 +1,61 @@
+package by.epam.andrewzenov.data.model;
+
+import java.time.LocalDateTime;
+
+public class User {
+
+	private String login;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private LocalDateTime createDate;
+	private LocalDateTime endDate;
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public LocalDateTime getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(LocalDateTime createDate) {
+		this.createDate = createDate;
+	}
+
+	public LocalDateTime getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDateTime endDate) {
+		this.endDate = endDate;
+	}
+}
